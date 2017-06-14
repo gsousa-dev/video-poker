0efa9c0d843955707a8ec5ef886eada74b96c2eb
Guilherme Sousa <gui.sousa96@gmail.com>
2017-06-14 01:02:43 +0100

    Created Transaction Controller and respective Model

---
+++ webapp/controller/TransactionController.php
@@ -0,0 +1,59 @@
+<?php
+
+use ArmoredCore\Controllers\BaseController;
+use ArmoredCore\WebObjects\Redirect;
+use ArmoredCore\WebObjects\Session;
+use ArmoredCore\WebObjects\View;
+use ArmoredCore\Interfaces\ResourceControllerInterface;
+
+class TransactionController extends BaseController
+{
+    /**
+     * @return mixed
+     */
+    public function index() 
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function create() {}
+
+    /**
+     * @return mixed
+     */
+    public function store() {}
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function show($id) 
+    {
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function edit($id) 
+    {
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function update($id) 
+    {
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function destroy($id) 
+    {
+    }
+}
---
+++ webapp/model/Transaction.php
@@ -0,0 +1,8 @@
+<?php
+
+use ActiveRecord\Model;
+
+class Transaction extends Model
+{
+    //
+}
